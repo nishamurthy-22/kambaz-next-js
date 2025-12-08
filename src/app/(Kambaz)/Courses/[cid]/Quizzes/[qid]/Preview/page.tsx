5abc3ca540883b8afb03f7365016eb7c4dee066f
Nisha Murthy D <nishamurthy22@gmail.com>
2025-12-07 20:16:09 -0500

    final fix

--- src/app/(Kambaz)/Courses/[cid]/Quizzes/[qid]/Preview/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Quizzes/[qid]/Preview/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect, useCallback } from "react";
 import { useRouter, useParams } from "next/navigation";
 import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../../../../../../store";
+import { RootState } from "../../../../../store";
 import { setQuizzes } from "../../reducer";
 import * as client from "../../../../client";
 import { Button, Form, Card, Alert } from "react-bootstrap";
